a8f7c215687c0909c36bb65d18d58757a5e6c777
Achmad soewardi <soewardiachmad40@gmail.com>
2024-06-06 04:36:46 +0700

    baangun versi 2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char const *argv[])
     cout << "hello, world!" << endl;
 
     cout << "Hello gesssasioaioasvkaivn\n";
+    cout << "bangn kata urng mah udh jam 04.36"<<endl;
     cout << "achmad soewardi"<<endl;
     cout<<"Pagi gess"<<endl;
     return 0;
